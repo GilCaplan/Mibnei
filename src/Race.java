4cabf9cbc80faa7a3d62af158baab077259f460e
GilCaplan <rockycaplan@gmail.com>
2024-03-04 14:44:30 +0200

    added initial files

---
+++ src/Race.java
@@ -0,0 +1,56 @@
+public class Race {
+    public void init()
+    {
+        throw new UnsupportedOperationException("not implemented");
+    }
+    public void addRunner(RunnerID id)
+    {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public void removeRunner(RunnerID id)
+    {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public void addRunToRunner(RunnerID id, float time)
+    {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public void removeRunFromRunner(RunnerID id, float time)
+    {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public RunnerID getFastestRunnerAvg()
+    {
+
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public RunnerID getFastestRunnerMin()
+    {
+
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public float getMinRun(RunnerID id)
+    {
+
+        throw new UnsupportedOperationException("not implemented");
+    }
+    public float getAvgRun(RunnerID id){
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public int getRankAvg(RunnerID id)
+    {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public int getRankMin(RunnerID id)
+    {
+        throw new UnsupportedOperationException("not implemented");
+    }
+}
